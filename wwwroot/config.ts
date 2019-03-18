a934e22c342630d7c681d662caeb005dbff9adc8
Dave Long <david@itbsoftware.co.za>
2019-03-18 09:45:16 +0200

    remove unused values

--- wwwroot/config.ts
+++ wwwroot/config.ts
@@ -1,19 +1,15 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
     apiUrl: typeof window !== 'undefined' && window.location ? window.location.href.substr(0, window.location.href.lastIndexOf('/') + 1) + 'api/' : 'https://wallet.plenteum.com/api/',
-	mainnetExplorerUrl: "http://block-explorer.plenteum.com/",
-	testnet: false,
+    //apiUrl: 'https://wallet.plenteum.com/api/', //temporary testing front end
+	mainnetExplorerUrl: "http://block-explorer.plenteum.com",
     coinUnitPlaces: 8,
     coinDisplayUnitPlaces: 2,
 	txMinConfirms: 20,         
 	txCoinbaseMinConfirms: 20, 
 	addressPrefix: 18181,
-	integratedAddressPrefix: 0,
-	addressPrefixTestnet: 33,
-	integratedAddressPrefixTestnet: 34,
-	subAddressPrefix: 52,
-	subAddressPrefixTestnet: 73,
-	feePerKB: new JSBigInt('0'), //for testnet its not used, as fee is dynamic.
+	integratedAddressPrefix: 18181,
+	feePerKB: new JSBigInt('0'), 
 	dustThreshold: new JSBigInt('1000000'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
 	defaultMixin: 0, // default value mixins
 	idleTimeout: 30,
--- wwwroot/d/config.d.ts
+++ wwwroot/d/config.d.ts
@@ -2,8 +2,6 @@ declare var config : {
     apiUrl: string,
     miningUrl : string,
 	mainnetExplorerUrl: string,
-	testnetExplorerUrl: string,
-	testnet: boolean,
 	coinUnitPlaces: number,
 	txMinConfirms: number,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
 	txCoinbaseMinConfirms: number, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
@@ -13,10 +11,6 @@ declare var config : {
 	coinUriPrefix: string,
 	addressPrefix: number,
 	integratedAddressPrefix: number,
-	addressPrefixTestnet: number,
-	integratedAddressPrefixTestnet: number,
-	subAddressPrefix: number,
-	subAddressPrefixTestnet: number,
 	feePerKB: any,
 	dustThreshold: any,
 	defaultMixin: number, // default mixin
