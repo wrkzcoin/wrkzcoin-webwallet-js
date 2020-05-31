20b52325aa94363b183918813fe1a61be7e918e6
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-06-01 06:25:59 +0700

    tab to space

--- wwwroot/d/config.d.ts
+++ wwwroot/d/config.d.ts
@@ -1,22 +1,23 @@
 declare var config : {
     apiUrl: string,
     miningUrl : string,
-	mainnetExplorerUrl: string,
-	coinUnitPlaces: number,
-	txMinConfirms: number,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
-	txCoinbaseMinConfirms: number, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-	coinSymbol: string,
-	openAliasPrefix: string,
-	coinName: string,
-	coinUriPrefix: string,
-	addressPrefix: number,
-	integratedAddressPrefix: number,
-	feePerKB: any,
-	dustThreshold: any,
-	defaultMixin: number, // default mixin
-	txChargeAddress: string,
-	idleTimeout: number,
-	idleWarningDuration: number,
-	maxBlockNumber: number,
-	avgBlockTime: number,
+    mainnetExplorerUrl: string,
+    coinUnitPlaces: number,
+    txMinConfirms: number,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
+    txCoinbaseMinConfirms: number, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
+    coinSymbol: string,
+    openAliasPrefix: string,
+    coinName: string,
+    coinUriPrefix: string,
+    addressPrefix: number,
+    integratedAddressPrefix: number,
+    feePerKB: any,
+    dustThreshold: any,
+    defaultMixin: number, // default mixin
+    txChargeAddress: string,
+    idleTimeout: number,
+    idleWarningDuration: number,
+    maxBlockNumber: number,
+    avgBlockTime: number,
+    donateAddressWrkz: string,
 };
